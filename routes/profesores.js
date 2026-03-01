e0410fb04505cab42f5167367e03c1797b78bed0
Juan Esteban <juanestebanlezcanotejada@gmail.com>
2026-02-28 21:14:26 -0500

    Se agrego los roles y las passwords

--- routes/profesores.js
+++ routes/profesores.js
@@ -1,67 +1,167 @@
 const express = require('express');
 const router = express.Router();
 
-let profesores = [
-    { id: 1, nombre: 'Mateo', email: 'mateo@sena.edu.co', materia: 'Programación', activo: true },
-    { id: 2, nombre: 'Alexis', email: 'Alexis@sena.edu.co', materia: 'Quimmica', activo: true },
-    { id: 3, nombre: 'Guillermo', email: 'Guillermo@sena.edu.co', materia: 'Matematicas', activo: true },
+const maestros = [
+    { id: 1, nombre: 'Carlos', apellido: 'Perez', especialidad: 'Programacion', email: 'carlos@gmail.com', telefono: 8503658743},
+    { id: 2, nombre: 'Melisa', apellido: 'Uribe', especialidad: 'Quimica', email: 'melisadiabla@gmail.com', telefono: 3789321904},
+    { id: 3, nombre: 'Guillermo', apellido: 'Zuluaga', especialidad: 'Matematicas', email: 'guilleremito@gmail.com', telefono: 4363856815},
+    { id: 4, nombre: 'Vicky', apellido: 'Gutierrez', especialidad: 'Investigacion', email: 'doñavicky@gmail.com', telefono: 5678910347}
 ];
 
-// GET - Obtener todos con filtro dinámico 
-router.get('/profesores', (req, res) => {
-    const idioma = req.headers['accept-language'] || 'es';
-    const { nombre, email, materia, activo } = req.query; // Query params 
-    
-    let filteredProfesores = profesores.filter(p => {
-        return (!nombre || p.nombre.toLowerCase().includes(nombre.toLowerCase())) &&
-               (!email || p.email.toLowerCase().includes(email.toLowerCase())) &&
-               (!materia || p.materia.toLowerCase().includes(materia.toLowerCase())) &&
-               (activo === undefined || p.activo.toString() === activo);
+//GET: todos los maestros
+router.get('/maestros', (req, res) => {
+    const apiKey = req.headers['password'];
+    if (!apiKey) {
+        return res.status(401).json({
+        success: false,
+        message: 'API key es requerida'
     });
-
-    res.json({ success: true, idioma_detectado: idioma, total: filteredProfesores.length, data: filteredProfesores });
+}
+    if (apiKey !== '12345') {
+        return res.status(403).json({
+        success: false,
+        message: 'Error la password no es correcta'
+    });
+}
+    const { nombre, apellido, especialidad, email, telefono } = req.query;
+    let filtradosMaestros = maestros.filter(p => {
+        return((!nombre || p.nombre.toLocaleLowerCase().includes(nombre.toLocaleLowerCase()))) &&
+        (!apellido || p.apellido.toLocaleLowerCase().includes(apellido.toLocaleLowerCase())) &&
+        (!especialidad || p.especialidad.toLocaleLowerCase().includes(especialidad.toLocaleLowerCase())) &&
+        (!email || p.email.toLocaleLowerCase().includes(email.toLocaleLowerCase())) &&
+        (!telefono || p.telefono === Number(telefono))
+    })
+    res.json({ success: true, Headers: {apiKey}, data: filtradosMaestros });
 });
 
-// GET - Por ID
-router.get('/profesores:id', (req, res) => {
-    const profesor = profesores.find(p => p.id === parseInt(req.params.id));
-    if (!profesor) return res.status(404).json({ success: false, message: 'Profesor no encontrado' });
-    res.json({ success: true, data: profesor });
-});
-
-// POST - Crear profesor
-router.post('/profesores', (req, res) => {
-    const { nombre, email, materia } = req.body;
-    
-    if (!nombre || !materia) {
-        return res.status(400).json({ success: false, message: 'Faltan campos obligatorios' });
+// GET: obtener un estudiante por ID
+router.get('/maestros/:id', (req, res) => {
+    const apiKey = req.headers['password'];
+    if (!apiKey) {
+        return res.status(401).json({
+        success: false,
+        message: 'API key es requerida'
+    });
+}
+    if (apiKey !== '12345') {
+        return res.status(403).json({
+        success: false,
+        message: 'Error la password no es correcta'
+    });
+}
+    const teacher = maestros.find(u => u.id === parseInt(req.params.id));
+    if (!teacher){
+        return res.status(404).json({ success: false, message: 'Maestro no encontrado' });
+    } else {
+        res.json({ success: true, Headers: {apiKey}, data: teacher });
     }
-
-    const nuevo = { id: profesores.length + 1, nombre, email, materia, activo: true };
-    profesores.push(nuevo);
-    res.status(201).json({ success: true, data: nuevo });
 });
 
-// PUT - Actualizar
-router.put('/profesores:id', (req, res) => {
-    const profesor = profesores.find(p => p.id === parseInt(req.params.id));
-    if (!profesor) return res.status(404).json({ success: false, message: 'No encontrado' });
-
-    const { nombre, materia, activo } = req.body;
-    if (nombre) profesor.nombre = nombre;
-    if (materia) profesor.materia = materia;
-    if (activo !== undefined) profesor.activo = activo;
-
-    res.json({ success: true, data: profesor });
+//POST: agregar maestro
+router.post('/maestros', (req, res) => {
+    const apiKey = req.headers['password'];
+    const role = req.headers['x-user-role'];
+    if (!apiKey) {
+        return res.status(401).json({
+        success: false,
+        message: 'API key es requerida'
+    });
+}
+    if (apiKey !== '6789') {
+        return res.status(403).json({
+        success: false,
+        message: 'Error la password no es correcta'
+    });
+}
+    if (role !== 'admin') {
+        return res.status(403).json({
+            success: false,
+            message: 'No tienes permisos para realizar esta acción'
+        });
+    }
+    const { nombre, apellido, especialidad, email, telefono} = req.body;
+    if (!nombre || !apellido || !especialidad || !email || !telefono) {
+        return res.status(400).json({ success: false, message: 'Faltan datos requeridos' });
+    }
+    const newMaestro = {
+        id: maestros.length + 1,
+        nombre,
+        apellido,
+        especialidad,
+        email,
+        telefono
+    };
+    maestros.push(newMaestro);
+    res.status(201).json({ success: true, Headers: {apiKey, role}, data: newMaestro });
 });
 
-// DELETE - Eliminar
-router.delete('/profesores:id', (req, res) => {
-    const index = profesores.findIndex(p => p.id === parseInt(req.params.id));
-    if (index === -1) return res.status(404).json({ success: false, message: 'No encontrado' });
+//PUT: actualizar datos
+router.put('/maestros/:id', (req, res) => {
+    const apiKey = req.headers['password'];
+    const role = req.headers['x-user-role'];
+    if (!apiKey) {
+        return res.status(401).json({
+        success: false,
+        message: 'API key es requerida'
+    });
+}
+    if (apiKey !== '6789') {
+        return res.status(403).json({
+        success: false,
+        message: 'Error la password no es correcta'
+    });
+}
+    if (role !== 'admin') {
+        return res.status(403).json({
+            success: false,
+            message: 'No tienes permisos para realizar esta acción'
+        });
+    }
+    const id = parseInt(req.params.id);
+    const { nombre, apellido, especialidad, email, telefono} = req.body;
+    const maestro = maestros.find(e => e.id === id);
+    if (!maestro) {
+        return res.status(404).json({ success: false, message: 'Maestro no encontrado' });
+    }
+    // Actualiza los campos
+    maestro.nombre = nombre;
+    maestro.apellido = apellido;
+    maestro.especialidad = especialidad;
+    maestro.email = email;
+    maestro.telefono = telefono
 
-    const eliminado = profesores.splice(index, 1);
-    res.json({ success: true, message: 'Profesor eliminado', data: eliminado });
+    res.json({ success: true, Headers: {apiKey, role}, data: maestro });
+});
+
+//DELETE: eliminar estudiante por ID
+router.delete('/maestros/:id', (req, res) => {
+    const apiKey = req.headers['password'];
+    const role = req.headers['x-user-role'];
+        if (!apiKey) {
+        return res.status(401).json({
+        success: false,
+        message: 'API key es requerida'
+    });
+}
+    if (apiKey !== '6789') {
+        return res.status(403).json({
+        success: false,
+        message: 'Error la password no es correcta'
+    });
+}
+    if (role !== 'admin') {
+        return res.status(403).json({
+            success: false,
+            message: 'No tienes permisos para realizar esta acción'
+        });
+    }
+    const teacher = maestros.findIndex(u => u.id === parseInt(req.params.id));
+    if (teacher === -1) {
+        return res.status(404).json({ success: false, message: 'Maestro no encontrado' });
+    }
+    maestros.splice(teacher, 1);
+    
+    res.status(201).json({ success: true, Headers: {apiKey, role}, data: "El maestro se ha eliminado" });
 });
 
 module.exports = router;
