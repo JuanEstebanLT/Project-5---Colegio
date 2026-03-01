2606d074cb8d3d69ec3095c854d6c43695dfd06e
Juan Esteban <juanestebanlezcanotejada@gmail.com>
2026-02-28 21:13:28 -0500

    Se agrego los roles y las passwords

--- routes/estudiantes.js
+++ routes/estudiantes.js
@@ -1,86 +1,163 @@
 const express = require('express');
 const router = express.Router();
 
-// Datos de ejemplo iniciales
-let estudiantes = [
-    { id: 1, nombre: 'Juan Perez', email: 'juan@sena.edu.co', grado: '10A', activo: true },
-    { id: 2, nombre: 'Maria Lopez', email: 'maria@sena.edu.co', grado: '11B', activo: true },
+const estudiantes = [
+    { id: 1, nombre: 'Ana', apellido: 'Gómez', genero: 'Femenino', email: 'anagomez@gmail.com'},
+    { id: 2, nombre: 'Pedro', apellido: 'Correa', genero: 'Masculino', email: 'pedrocorreita@gmail.com'},
+    { id: 3, nombre: 'Sara', apellido: 'Bolivar', genero: 'Femenino', email: 'sarabolivar@gmail.com'}
 ];
- 
-// GET - Obtener todos (Incluye Filtro Query Params) 
+
+//GET: todos los estudiantes
 router.get('/estudiantes', (req, res) => {
-    const { nombre, email, grado, activo } = req.query; // Query params 
-    
-    let filteredEstudiantes = estudiantes.filter(e => {
-        return (!nombre || e.nombre.toLowerCase().includes(nombre.toLowerCase())) &&
-               (!email || e.email.toLowerCase().includes(email.toLowerCase())) &&
-               (!grado || e.grado.toLowerCase().includes(grado.toLowerCase())) &&
-               (activo === undefined || e.activo.toString() === activo);
+    const apiKey = req.headers['password'];
+    if (!apiKey) {
+        return res.status(401).json({
+        success: false,
+        message: 'API key es requerida'
     });
-
-    // ¡Aquí devolvemos la variable filtrada, no el arreglo original!
-    res.json({ success: true, total: filteredEstudiantes.length, data: filteredEstudiantes });
+}
+    if (apiKey !== '12345') {
+        return res.status(403).json({
+        success: false,
+        message: 'Error la password no es correcta'
+    });
+}
+    const { nombre, apellido, genero, email} = req.query;
+    let filtradosEstudiantes = estudiantes.filter(p => {
+        return((!nombre || p.nombre.toLocaleLowerCase().includes(nombre.toLocaleLowerCase()))) &&
+        (!apellido || p.apellido.toLocaleLowerCase().includes(apellido.toLocaleLowerCase())) &&
+        (!genero || p.genero.toLocaleLowerCase().includes(genero.toLocaleLowerCase())) &&
+        (!email || p.email.toLocaleLowerCase().includes(email.toLocaleLowerCase()))
+    })
+    res.json({ success: true, Headers: {apiKey}, data: filtradosEstudiantes });
 });
 
-// GET - Obtener por ID 
+// GET: obtener un estudiante por ID
 router.get('/estudiantes/:id', (req, res) => {
-    const estudiante = estudiantes.find(e => e.id === parseInt(req.params.id));
-    
-    if (!estudiante) {
-        return res.status(404).json({ success: false, message: 'Estudiante no encontrado' }); 
+    const apiKey = req.headers['password'];
+    if (!apiKey) {
+        return res.status(401).json({
+        success: false,
+        message: 'API key es requerida'
+    });
+}
+    if (apiKey !== '12345') {
+        return res.status(403).json({
+        success: false,
+        message: 'Error la password no es correcta'
+    });
+}
+    const estudent = estudiantes.find(u => u.id === parseInt(req.params.id));
+    if (!estudent) {
+        return res.status(404).json({ success: false, message: 'Estudiante no encontrado' });
+    } else {
+        res.json({ success: true, Headers: {apiKey}, data: estudent });
     }
-    
-    res.json({ success: true, data: estudiante }); 
 });
 
-// POST - Crear estudiante (Incluye validación de Header) 
+//POST: agregar estudiante
 router.post('/estudiantes', (req, res) => {
-    // Lectura de Header obligatorio para este endpoint
-    const auth = req.headers['authorization'];
-
-    if (!auth) {
-        return res.status(400).json({ success: false, message: 'Header Authorization es requerido' }); 
+    const apiKey = req.headers['password'];
+    const role = req.headers['x-user-role'];
+    if (!apiKey) {
+        return res.status(401).json({
+        success: false,
+        message: 'API key es requerida'
+    });
+}
+    if (apiKey !== '6789') {
+        return res.status(403).json({
+        success: false,
+        message: 'Error la password no es correcta'
+    });
+}
+    if (role !== 'admin') {
+        return res.status(403).json({
+            success: false,
+            message: 'No tienes permisos para realizar esta acción'
+        });
     }
-
-    const { nombre, email, grado } = req.body;
-    
-    if (!nombre || !email) {
-        return res.status(400).json({ success: false, message: 'Datos incompletos' });
+    const { nombre, apellido, genero, email} = req.body;
+    if (!nombre || !apellido || !genero || !email) {
+        return res.status(400).json({ success: false, message: 'Faltan datos requeridos' });
     }
-
-    const nuevo = {
+    const newEstudiante = {
         id: estudiantes.length + 1,
         nombre,
+        apellido,
+        genero,
         email,
-        grado,
-        activo: true
     };
-
-    estudiantes.push(nuevo);
-    res.status(201).json({ success: true, data: nuevo }); 
+    estudiantes.push(newEstudiante);
+    res.status(201).json({ success: true, Headers: {apiKey, role}, data: newEstudiante });
 });
 
-// PUT - Actualizar por ID 
-router.put('/estudiantes:id', (req, res) => {
-    const estudiante = estudiantes.find(e => e.id === parseInt(req.params.id));
-    if (!estudiante) return res.status(404).json({ success: false, message: 'No encontrado' });
+//PUT: actualizar datos
+router.put('/estudiantes/:id', (req, res) => {
+    const apiKey = req.headers['password'];
+    const role = req.headers['x-user-role'];
+    if (!apiKey) {
+        return res.status(401).json({
+        success: false,
+        message: 'API key es requerida'
+    });
+}
+    if (apiKey !== '6789') {
+        return res.status(403).json({
+        success: false,
+        message: 'Error la password no es correcta'
+    });
+}
+    if (role !== 'admin') {
+        return res.status(403).json({
+            success: false,
+            message: 'No tienes permisos para realizar esta acción'
+        });
+    }
+    const id = parseInt(req.params.id);
+    const { nombre, apellido, genero, email } = req.body;
+    const estudiante = estudiantes.find(e => e.id === id);
+    if (!estudiante) {
+        return res.status(404).json({ success: false, message: 'Estudiante no encontrado' });
+    }
+    // Actualiza los campos
+    estudiante.nombre = nombre;
+    estudiante.apellido = apellido;
+    estudiante.genero = genero;
+    estudiante.email = email;
 
-    const { nombre, email, grado, activo } = req.body;
-    if (nombre) estudiante.nombre = nombre;
-    if (email) estudiante.email = email;
-    if (grado) estudiante.grado = grado;
-    if (activo !== undefined) estudiante.activo = activo;
-
-    res.json({ success: true, data: estudiante });
+    res.json({ success: true, Headers: {apiKey, role}, data: estudiante });
 });
 
-// DELETE - Eliminar por ID
-router.delete('/estudiantes:id', (req, res) => {
-    const index = estudiantes.findIndex(e => e.id === parseInt(req.params.id));
-    if (index === -1) return res.status(404).json({ success: false, message: 'No encontrado' });
-
-    const eliminado = estudiantes.splice(index, 1);
-    res.json({ success: true, message: 'Estudiante eliminado', data: eliminado });
+//DELETE: eliminar estudiante por ID
+router.delete('/estudiantes/:id', (req, res) => {
+    const apiKey = req.headers['password'];
+    const role = req.headers['x-user-role'];
+        if (!apiKey) {
+        return res.status(401).json({
+        success: false,
+        message: 'API key es requerida'
+    });
+}
+    if (apiKey !== '6789') {
+        return res.status(403).json({
+        success: false,
+        message: 'Error la password no es correcta'
+    });
+}
+    if (role !== 'admin') {
+        return res.status(403).json({
+            success: false,
+            message: 'No tienes permisos para realizar esta acción'
+        });
+    }
+    const student = estudiantes.findIndex(u => u.id === parseInt(req.params.id));
+    if (student === -1) {
+        return res.status(404).json({ success: false, message: 'Estudiante no encontrado' });
+    }
+    estudiantes.splice(student, 1);
+    
+    res.status(201).json({ success: true, Headers: {apiKey, role}, data: "El estudiante se ha eliminado" });
 });
 
 module.exports = router;
